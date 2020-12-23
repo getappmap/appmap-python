8357c30548891bec7e6aff7b4cf692de72c173d7
Alan Potter <alanjpotter@gmail.com>
2020-12-23 04:54:19 -0500

    Apply suggestions from code review
    Co-authored-by: Viraj Kanwade <virajkanwade@users.noreply.github.com>

--- appmap/_implementation/py_version_check.py
+++ appmap/_implementation/py_version_check.py
@@ -8,9 +8,6 @@ class AppMapPyVerException(Exception):
 
 
 def check_py_version():
-    cur_py_version = [int(x) for x in platform.python_version_tuple()]
-
-    min_py_version = [int(x) for x in str(MIN_PY_VERSION).split('.')]
-
-    if cur_py_version[0] < min_py_version[0] or cur_py_version[1] < min_py_version[1]:
+    MIN_PY_VERSION = (3, 8)
+    if sys.version_info < MIN_PY_VERSION:
         raise AppMapPyVerException(f'Minimum Python version supported is {MIN_PY_VERSION}, found {platform.python_version()}')
