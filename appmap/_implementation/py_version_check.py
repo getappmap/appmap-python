71f37993a8958bf8b69c8bef8edbf8d980252f6f
Alan Potter <alanjpotter@gmail.com>
2021-02-15 17:21:09 -0500

    Bump required Python version to 3.9

--- appmap/_implementation/py_version_check.py
+++ appmap/_implementation/py_version_check.py
@@ -7,6 +7,6 @@ class AppMapPyVerException(Exception):
 
 
 def check_py_version():
-    MIN_PY_VERSION = (3, 8)
+    MIN_PY_VERSION = (3, 9)
     if sys.version_info < MIN_PY_VERSION:
         raise AppMapPyVerException(f'Minimum Python version supported is {MIN_PY_VERSION}, found {platform.python_version()}')
