a6da99f73a4ba8810c2f98da97a4df321465305e
Dustin Byrne <dustinsbyrne@gmail.com>
2021-04-14 18:02:58 -0400

    test: Test for correct message array on HTTP request

--- appmap/test/test_django.py
+++ appmap/test/test_django.py
@@ -48,3 +48,85 @@ def test_http_capture(events):
         'status_code': 404,
         'mime_type': 'text/html'
     }
+
+
+def test_message_capture_get(events):
+    client = django.test.Client()
+    client.get('/test', { 'my_param': 'example' }, content_type='application/x-www-form-urlencoded')
+
+    assert events[0].message == [
+        {
+            'name': 'my_param',
+            'class': 'builtins.str',
+            'object_id': events[0].message[0]['object_id'],
+            'value': 'example'
+        }
+    ]
+
+def test_message_capture_get_arr(events):
+    client = django.test.Client()
+    client.get('/test', { 'my_param': ['example', 'example2'] }, content_type='application/x-www-form-urlencoded')
+
+    assert events[0].message == [
+        {
+            'name': 'my_param',
+            'class': 'builtins.list',
+            'object_id': events[0].message[0]['object_id'],
+            'value': "['example', 'example2']"
+        },
+    ]
+
+
+def test_message_capture_post_form_urlencoded(events):
+    client = django.test.Client()
+    client.generic('POST', '/test?my_param=example', content_type='application/x-www-form-urlencoded')
+
+    assert events[0].message == [
+        {
+            'name': 'my_param',
+            'class': 'builtins.str',
+            'object_id': events[0].message[0]['object_id'],
+            'value': 'example'
+        }
+    ]
+
+def test_message_capture_put(events):
+    client = django.test.Client()
+    client.put('/test', { 'my_param': 'example' }, content_type='application/json')
+
+    assert events[0].message == [
+        {
+            'name': 'my_param',
+            'class': 'builtins.str',
+            'object_id': events[0].message[0]['object_id'],
+            'value': 'example'
+        }
+    ]
+
+
+def test_message_capture_post_json(events):
+    client = django.test.Client()
+    client.post('/test', { 'my_param': 'example' }, content_type='application/json')
+
+    assert events[0].message == [
+        {
+            'name': 'my_param',
+            'class': 'builtins.str',
+            'object_id': events[0].message[0]['object_id'],
+            'value': 'example'
+        }
+    ]
+
+
+def test_message_capture_post_multipart(events):
+    client = django.test.Client()
+    client.post('/test', { 'my_param': 'example' }, content_type=django.test.client.MULTIPART_CONTENT)
+
+    assert events[0].message == [
+        {
+            'name': 'my_param',
+            'class': 'builtins.str',
+            'object_id': events[0].message[0]['object_id'],
+            'value': 'example'
+        }
+    ]
