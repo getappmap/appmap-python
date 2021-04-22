71b409b1b83aad21e230b6c31719c48b10d51bb1
Alan Potter <alanjpotter@gmail.com>
2021-04-22 07:00:52 -0400

    Make sure django tests run with a clean slate
    Wrap the django tests in a class that inherits from AppMapTestBase. This
    ensures that everything (e.g. environment variables) will be
    reinitialized before the test is run.

--- appmap/test/test_django.py
+++ appmap/test/test_django.py
@@ -4,6 +4,8 @@
 import pytest
 from appmap._implementation.recording import Recorder
 
+from .appmap_test_base import AppMapTestBase
+
 pytest.importorskip('django')
 
 import django.conf
@@ -11,11 +13,15 @@ import django.db
 import django.test
 import appmap.django  # noqa: F401
 
+
+# Note that with django configured this way, django.test.Client
+# requests for '/test' will print warning messages that look like
+# `WARNING:django.request:Not Found: /test`. These don't appear to
+# indicate a problem.
 django.conf.settings.configure(
     DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:'}},
     ROOT_URLCONF=()
 )
-
 
 @pytest.fixture
 def events():
@@ -26,151 +32,151 @@ def events():
     rec.enabled = False
     rec.events().clear()
 
+class TestDjango(AppMapTestBase):
+    def test_sql_capture(self, events):
+        conn = django.db.connections['default']
+        conn.cursor().execute('SELECT 1').fetchall()
 
-def test_sql_capture(events):
-    conn = django.db.connections['default']
-    conn.cursor().execute('SELECT 1').fetchall()
-
-    assert events[0].sql_query['sql'] == 'SELECT 1'
-
-
-def test_http_capture(events):
-    client = django.test.Client()
-    client.get('/test')
-
-    assert events[0].http_server_request.items() >= {
-        'request_method': 'GET',
-        'path_info': '/test',
-        'protocol': 'HTTP/1.1'
-    }.items()
-
-    assert events[1].http_server_response == {
-        'status_code': 404,
-        'mime_type': 'text/html'
-    }
+        assert events[0].sql_query['sql'] == 'SELECT 1'
 
 
-def test_message_capture_post(events):
-    client = django.test.Client()
-    client.post('/test', { 'my_param': 'example' },
-        content_type='application/json; charset=UTF-8',
-        HTTP_AUTHORIZATION='token "test-token"',
-        HTTP_ACCEPT='application/json',
-        HTTP_ACCEPT_LANGUAGE='pl'
-    )
+    def test_http_capture(self, events):
+        client = django.test.Client()
+        client.get('/test')
 
-    assert events[0].message == [
-        {
-            'name': 'my_param',
-            'class': 'builtins.str',
-            'object_id': events[0].message[0]['object_id'],
-            'value': 'example'
+        assert events[0].http_server_request.items() >= {
+            'request_method': 'GET',
+            'path_info': '/test',
+            'protocol': 'HTTP/1.1'
+        }.items()
+
+        assert events[1].http_server_response == {
+            'status_code': 404,
+            'mime_type': 'text/html'
         }
-    ]
-    assert events[0].http_server_request.items() >= {
-        'request_method': 'POST',
-        'path_info': '/test',
-        'protocol': 'HTTP/1.1',
-        'authorization': 'token "test-token"',
-        'mime_type': 'application/json; charset=UTF-8',
-    }.items()
-
-    assert events[0].http_server_request['headers'].items() >= {
-        'Accept': 'application/json',
-        'Accept-Language': 'pl'
-    }.items()
-
-def test_message_capture_get(events):
-    client = django.test.Client()
-    client.get('/test', { 'my_param': 'example' })
-
-    assert events[0].message == [
-        {
-            'name': 'my_param',
-            'class': 'builtins.str',
-            'object_id': events[0].message[0]['object_id'],
-            'value': 'example'
-        }
-    ]
-
-def test_message_capture_get_arr(events):
-    client = django.test.Client()
-    client.get('/test', { 'my_param': ['example', 'example2'] })
-
-    assert events[0].message == [
-        {
-            'name': 'my_param',
-            'class': 'builtins.list',
-            'object_id': events[0].message[0]['object_id'],
-            'value': "['example', 'example2']"
-        },
-    ]
 
 
-def test_message_capture_post_form_urlencoded(events):
-    client = django.test.Client()
-    client.post('/test', 'my_param=example', content_type='application/x-www-form-urlencoded')
+    def test_message_capture_post(self, events):
+        client = django.test.Client()
+        client.post('/test', { 'my_param': 'example' },
+            content_type='application/json; charset=UTF-8',
+            HTTP_AUTHORIZATION='token "test-token"',
+            HTTP_ACCEPT='application/json',
+            HTTP_ACCEPT_LANGUAGE='pl'
+        )
 
-    assert events[0].message == [
-        {
-            'name': 'my_param',
-            'class': 'builtins.str',
-            'object_id': events[0].message[0]['object_id'],
-            'value': 'example'
-        }
-    ]
+        assert events[0].message == [
+            {
+                'name': 'my_param',
+                'class': 'builtins.str',
+                'object_id': events[0].message[0]['object_id'],
+                'value': 'example'
+            }
+        ]
+        assert events[0].http_server_request.items() >= {
+            'request_method': 'POST',
+            'path_info': '/test',
+            'protocol': 'HTTP/1.1',
+            'authorization': 'token "test-token"',
+            'mime_type': 'application/json; charset=UTF-8',
+        }.items()
 
-def test_message_capture_put(events):
-    client = django.test.Client()
-    client.put('/test', { 'my_param': 'example' }, content_type='application/json')
+        assert events[0].http_server_request['headers'].items() >= {
+            'Accept': 'application/json',
+            'Accept-Language': 'pl'
+        }.items()
 
-    assert events[0].message == [
-        {
-            'name': 'my_param',
-            'class': 'builtins.str',
-            'object_id': events[0].message[0]['object_id'],
-            'value': 'example'
-        }
-    ]
+    def test_message_capture_get(self, events):
+        client = django.test.Client()
+        client.get('/test', { 'my_param': 'example' })
 
+        assert events[0].message == [
+            {
+                'name': 'my_param',
+                'class': 'builtins.str',
+                'object_id': events[0].message[0]['object_id'],
+                'value': 'example'
+            }
+        ]
 
-def test_message_capture_post_bad_json(events):
-    client = django.test.Client()
-    client.post('/test?my_param=example', "bad json", content_type='application/json')
+    def test_message_capture_get_arr(self, events):
+        client = django.test.Client()
+        client.get('/test', { 'my_param': ['example', 'example2'] })
 
-    assert events[0].message == [
-        {
-            'name': 'my_param',
-            'class': 'builtins.str',
-            'object_id': events[0].message[0]['object_id'],
-            'value': 'example'
-        }
-    ]
-
-
-def test_message_capture_post_multipart(events):
-    client = django.test.Client()
-    client.post('/test', { 'my_param': 'example' })
-
-    assert events[0].message == [
-        {
-            'name': 'my_param',
-            'class': 'builtins.str',
-            'object_id': events[0].message[0]['object_id'],
-            'value': 'example'
-        }
-    ]
+        assert events[0].message == [
+            {
+                'name': 'my_param',
+                'class': 'builtins.list',
+                'object_id': events[0].message[0]['object_id'],
+                'value': "['example', 'example2']"
+            },
+        ]
 
 
-def test_message_capture_post_with_query(events):
-    client = django.test.Client()
-    client.post('/test?my_param=get', { 'my_param': 'example' })
+    def test_message_capture_post_form_urlencoded(self, events):
+        client = django.test.Client()
+        client.post('/test', 'my_param=example', content_type='application/x-www-form-urlencoded')
 
-    assert events[0].message == [
-        {
-            'name': 'my_param',
-            'class': 'builtins.list',
-            'object_id': events[0].message[0]['object_id'],
-            'value': "['get', 'example']"
-        }
-    ]
+        assert events[0].message == [
+            {
+                'name': 'my_param',
+                'class': 'builtins.str',
+                'object_id': events[0].message[0]['object_id'],
+                'value': 'example'
+            }
+        ]
+
+    def test_message_capture_put(self, events):
+        client = django.test.Client()
+        client.put('/test', { 'my_param': 'example' }, content_type='application/json')
+
+        assert events[0].message == [
+            {
+                'name': 'my_param',
+                'class': 'builtins.str',
+                'object_id': events[0].message[0]['object_id'],
+                'value': 'example'
+            }
+        ]
+
+
+    def test_message_capture_post_bad_json(self, events):
+        client = django.test.Client()
+        client.post('/test?my_param=example', "bad json", content_type='application/json')
+
+        assert events[0].message == [
+            {
+                'name': 'my_param',
+                'class': 'builtins.str',
+                'object_id': events[0].message[0]['object_id'],
+                'value': 'example'
+            }
+        ]
+
+
+    def test_message_capture_post_multipart(self, events):
+        client = django.test.Client()
+        client.post('/test', { 'my_param': 'example' })
+
+        assert events[0].message == [
+            {
+                'name': 'my_param',
+                'class': 'builtins.str',
+                'object_id': events[0].message[0]['object_id'],
+                'value': 'example'
+            }
+        ]
+
+
+    def test_message_capture_post_with_query(self, events):
+        client = django.test.Client()
+        client.post('/test?my_param=get', { 'my_param': 'example' })
+
+        assert events[0].message == [
+            {
+                'name': 'my_param',
+                'class': 'builtins.list',
+                'object_id': events[0].message[0]['object_id'],
+                'value': "['get', 'example']"
+            }
+        ]
