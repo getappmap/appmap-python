a7894efbf7d45ab54119ff12aa5b5ddb9757ee79
Alan Potter <alanjpotter@gmail.com>
2021-07-09 08:20:41 -0400

    Factor out test django app
    Move the test django app out of test_django.py into its own package.
    This makes it possible for it to have its own tests.

---
+++ appmap/test/data/django/test/test_app.py
@@ -0,0 +1,21 @@
+from django.core.handlers.base import BaseHandler
+
+import app
+
+from django.test import Client
+
+def test_middleware(rf, settings):
+    settings.MIDDLEWARE = [
+        "app.middleware.hello_world"
+    ]
+    request = rf.get('/')
+    handler = BaseHandler()
+    handler.load_middleware()
+    response = handler.get_response(request)
+    assert response.status_code == 200
+
+def test_app(settings):
+    response = Client().get('/')
+
+    assert response.status_code == 200
+
