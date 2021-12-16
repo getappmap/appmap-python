710dda28c628b33caf8611a59b8a72f652d96ab5
Rafał Rzepecki <rafal@app.land>
2021-12-16 09:45:14 -0500

    feat: Preset labels for known library functions
    Currently just yaml, more to come.

---
+++ appmap/labeling/__init__.py
@@ -0,0 +1,17 @@
+""" This module provides predefined labels for some common library functions.
+For consistency, the labels are defined in YAML data files in this module,
+structured exactly like the labels section in appmap.yml.
+"""
+
+from importlib_resources import files
+import yaml
+
+from .._implementation.labels import LabelSet
+
+def presets() -> LabelSet:
+    """ Load the LabelSet of the presets included with appmap-python. """
+    labels = LabelSet()
+    for resource in files(__name__).iterdir():
+        if resource.suffix == '.yml':
+            labels.append(yaml.safe_load(resource.read_text()))
+    return labels
