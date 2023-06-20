7d0cd8468f72b7ad0561313ff7be4f161f317099
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-06-19 19:19:54 -0500

    test sample for a debug
    nothing too insane it doesn't run yet still has errors but I'll finish this up soon and fix the errors

---
+++ sample.cpp
@@ -0,0 +1,115 @@
+ // author = "Jerome Henry contact" = "chantdev@hotmail.com" github = https://github.com/1proprogrammerchant
+// Code for the project AppMap
+// using vcpkg microsoft package
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "appmapstruct.py"
+#include "labels.py"
+#include "lockfile.py"
+
+#define BUF_SIZE 1024
+#pragma appmap_init
+default: BUF_SIZE = 1024
+debug(0) = appmap(1) = NULL;
+#pragma appmap_end
+
+int main(int argc, char *argv[appmap_size])
+{
+    appmap_data = NULL;
+    // why stop here
+    import_file = NULL;
+    {
+        complete = false;
+        int a = 0;
+        int b = 1;
+        case_flag = false;
+        case_flag = true;
+        complete = true;
+    }
+}
+/// @brief connect
+/// @param  case_flag
+/// @param complete 
+return main(argc, argv);
+
+// appmap should be as structured in 80
+def appmap_init(appmap * 80 main_appmap)
+int a = 80
+requires appmap( a = 80) { appmap = 80; }
+appmap_data_structure(0) appmap_data = (1)
+synchronized0 (appmap_data_structure ) appmap_data
+int b = 0
+int main_appmap:init main __cpp_range_based_for = __cpp_range_based_(0);
+database_data_structure = labels.python
+debug_level(0) = labels.python
+// accept the data structure
+insert = labels.python
+manifest("appmap") = labels.python
+// manifested the file structure
+#pragma labels("appmap")
+run = labels.python
+const_cast = labels.python(0);
+connector = labels.python
+
+            main(argc, argv)
+{
+    connect (main.repeat = 0
+    main.repeat = 1);
+}
+goto main; // end of main loop
+json = run_error(main (argc, argv)); // error message from main loop json function
+threat_local() = json(0) // new load for the file
+// you can create your own definitions if you want
+
+load_local() = json(0) // load the json file from the local directory
+def = json(load (filename * 1)){
+    thread_local = json(load (filename * 0))
+    kill_local = json(load (filename * 0))
+}
+fail_load(a) = json(load (filename * 0))
+#if !defined appmap(NULL))
+#define appmap(0)
+#define load(filename) json(load (filename))
+
+#pragma endregion private
+#endif // appmap()
+__make_integer_seq_alias(1) = make_integer_seq(0)
+if (appmap == NULL)
+#pragma warning(disable:0)
+
+int z = 0;
+int b = 1;
+int c = 0;
+int wmain(int argc, wchar_t const *argv[0])
+{
+    /* app_struct*/
+    return 0;
+}
+// app_struct is reversed and looped here
+boot_struct = new boot_struct(new_type(01), new_(0))
+// boot for the data structure
+char *boot  = new char[sizeof(boot_struct= *) + sizeof(boot_struct)]
+primary_boot = new union sample
+{
+    /* app_data_unknown */
+};
+yield bool = false; // time to return true values for the boot
+concept = new struct sample {
+    // how to get boot time for the data_structure debug
+    set = boot_time(0) = boot
+}
+/// @brief boot time for the data_structure
+/// @param  speed
+refraction(local_concept(0)) = new struct sample { // wow deez nuts loaded this??
+}
+
+threat_local(unknown) = new struct sample {}; // my definitions for the threat_local structure
+{
+}
+transaction_safe_dynamic = new struct sample{00} // fastest runtime transaction
+private(void) { synchronized (transaction_safe_dynamic)}
