84c3b484353433d4c524c412a199d97bb6830729
Alan Potter <alanjpotter@gmail.com>
2024-05-12 07:49:45 -0400

    fix: combine testing-related env vars
    Combine APPMAP_RECORD_PYTEST and APPMAP_RECORD_UNITTEST into
    APPMAP_RECORD_TESTS.

--- appmap/unittest.py
+++ appmap/unittest.py
@@ -2,6 +2,6 @@ from _appmap.env import Env
 
 logger = Env.current.getLogger(__name__)
 
-if not Env.current.is_appmap_repo and Env.current.enables("unittest"):
+if not Env.current.is_appmap_repo and Env.current.enables("tests"):
     logger.debug("Test recording is enabled (unittest)")
-    import _appmap.unittest  # pyright: ignore pylint: disable=unused-import
+    import _appmap.unittest  # pyright: ignore pylint: disable=unused-import  # noqa: F401
