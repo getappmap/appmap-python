61d9aeec38ca677db19b9132c72749d8cf0a6a73
zermelo-wisen <zermelo@wisen.tech>
2024-05-23 15:39:48 +0300

    test: non json serializable types

--- _appmap/test/data/pytest/simple.py
+++ _appmap/test/data/pytest/simple.py
@@ -1,3 +1,8 @@
+import numpy
+
+zero = numpy.int64(0)
+one = numpy.int64(1)
+
 class Simple:
     def hello(self):
         return "Hello"
@@ -5,5 +10,9 @@ class Simple:
     def world(self):
         return "world!"
 
+    def get_non_json_serializable(self):
+        return { zero: self.hello(), one: self.world() }
+
     def hello_world(self):
-        return "%s %s" % (self.hello(), self.world())
+        result = self.get_non_json_serializable()
+        return "%s %s" % (result[zero], result[one])
